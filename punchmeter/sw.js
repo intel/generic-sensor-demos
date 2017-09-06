c22bb364aa629ff707d85eb6cfa88bf8ce4e8e8d
Alexander Shalamov <alexander.shalamov@intel.com>
2017-09-06 13:56:15 +0300

    Add graphics, animation and SW (#8)

---
+++ punchmeter/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "punchmeter";
+const CACHE_VERSION = 1;
+
+self.addEventListener('install', function(event) {  
+  event.waitUntil(
+    caches.open(CACHE_NAME + CACHE_VERSION.toString()).then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'sw.js',
+        'gauge.js',
+        'main.js',
+        'styles.css',
+        'images/punchometer.png',
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+//Retrieval from cache
+        event.respondWith(caches.match(event.request).then(function(response) {
+                if (response !== undefined) {       //Found match in cache
+                        return response;
+                } else {
+                        return fetch(event.request).then(function (response) {
+                //Response may be used only once so need to clone it for reuse
+                let responseClone = response.clone();
+                caches.open(CACHE_VERSION.toString()).then(function (cache) {
+                        cache.put(event.request, responseClone);
+                });
+                return response;
+                });
+                }
+        }));
+});
+
