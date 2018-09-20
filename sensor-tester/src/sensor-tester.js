d94de72b0db39cacc093d8a97f46466ca5f722bc
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2018-09-20 14:15:38 +0200

    Move to Polymer 3.0, and start move to LitElement + MWC (#60)
    * Move to Polymer 3.0, and start move to LitElement + MWC
    
    * Move away from paper- and iron-elements
    
    * Lazy load pages
    
    * Fix basePath
    
    * Lazy load images
    
    * UI/UX Changes
    
    * Get rid of createPage and load JSON when visible
    
    * Use imperative drawer API

---
+++ sensor-tester/src/sensor-tester.js
@@ -0,0 +1,199 @@
+import "./sensor-tests-page.js";
+
+import '@polymer/app-layout/app-drawer/app-drawer';
+import '@polymer/app-layout/app-drawer-layout/app-drawer-layout';
+import '@polymer/app-layout/app-header/app-header';
+import '@polymer/app-layout/app-header-layout/app-header-layout';
+import '@polymer/app-layout/app-scroll-effects/app-scroll-effects';
+import '@polymer/app-layout/app-toolbar/app-toolbar';
+import {LitElement, html} from '@polymer/lit-element';
+import {installRouter} from '../node_modules/pwa-helpers/router.js';
+
+const menuIcon = html`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>`;
+
+class SensorTester extends LitElement {
+  static get properties() {
+    return {
+      page: {type: String}
+    };
+  }
+
+  constructor() {
+    super();
+    installRouter(location => {
+      const pathname = decodeURIComponent(location.pathname);
+
+      const parts = pathname.slice(1).split('/');
+      this.page = parts[parts.length - 1] || 'accelerometer';
+      if (this.drawer && !this.drawer.persistent) {
+        this.drawer.close();
+      }
+    });
+  }
+
+  firstUpdated() {
+    this.drawer = this.shadowRoot.querySelector('#drawer');
+  }
+
+  render() {
+    return html`
+      <style>
+        :host {
+          --app-primary-color: #0071c5;
+          --app-secondary-color: black;
+          display: block;
+        }
+
+        app-header {
+          color: #fff;
+          background-color: var(--app-primary-color);
+        }
+
+        app-drawer {
+          z-index: 2;
+        }
+
+        app-drawer-layout:not([narrow]) [drawer-toggle] {
+          display: none;
+        }
+
+        .drawer-list > a[selected] {
+          color: var(--app-drawer-selected-color);
+          font-weight: bold;
+        }
+
+        .drawer-list a {
+          display: block;
+          text-decoration: none;
+          color: var(--app-secondary-color);
+          padding: 0 18px;
+          height: 52px;
+        }
+
+        .page {
+          display: none;
+        }
+
+        .page[active] {
+          display: block;
+        }
+
+        .menu-btn {
+          display: inline-block;
+          width: 40px;
+          height: 40px;
+          padding: 8px;
+          box-sizing: border-box;
+          background: none;
+          border: none;
+          fill: var(--app-header-text-color);
+          cursor: pointer;
+          text-decoration: none;
+        }
+      </style>
+
+      <app-drawer-layout fullbleed>
+        <app-drawer id="drawer" slot="drawer">
+          <app-toolbar>Choose sensor:</app-toolbar>
+          <nav class="drawer-list">
+            <a ?selected="${this.page === 'accelerometer'}" href="accelerometer">
+              Accelerometer<br><small>device coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'accelerometer-screen'}" href="accelerometer-screen">
+              Accelerometer<br><small>screen coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'linearaccelerationsensor'}" href="linearaccelerationsensor">
+              LinearAccelerationSensor<br><small>device coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'linearaccelerationsensor-screen'}" href="linearaccelerationsensor-screen">
+              LinearAccelerationSensor<br><small>screen coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'gyroscope'}" href="gyroscope">
+              Gyroscope<br><small>device coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'absoluteorientationsensor'}" href="absoluteorientationsensor">
+              AbsoluteOrientationSensor<br><small>device coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'relativeorientationsensor'}" href="relativeorientationsensor">
+              RelativeOrientationSensor<br><small>device coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'magnetometer'}" href="magnetometer">
+              Magnetometer<br><small>device coordinates</small>
+            </a>
+            <a ?selected="${this.page === 'ambientlightsensor'}" href="ambientlightsensor">
+              AmbientLightSensor<br><small>device coordinates</small>
+            </a>
+          </nav>
+        </app-drawer>
+
+        <app-header-layout has-scrolling-region>
+          <app-header slot="header" condenses reveals effects="waterfall">
+            <app-toolbar>
+              <button class="menu-btn" aria-label="Menu" drawer-toggle
+                @click="${(ev) => {
+                  this.drawer.toggle();
+                  // Stop propagation as a click outside drawer will close it!
+                  ev.stopPropagation();
+                }}">${menuIcon}</button>
+              <div main-title>Sensor tester</div>
+            </app-toolbar>
+          </app-header>
+
+          <main role="main" class="main-content">
+            <sensor-tests-page class="page"
+              type="Accelerometer"
+              src="src/tests/accelerometer.json"
+              ?active="${this.page === 'accelerometer'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="Accelerometer"
+              src="src/tests/accelerometer-screen.json"
+              referenceFrame="screen"
+              ?active="${this.page === 'accelerometer-screen'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="LinearAccelerationSensor"
+              src="src/tests/linearaccelerationsensor.json"
+              ?active="${this.page === 'linearaccelerationsensor'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="LinearAccelerationSensor"
+              src="src/tests/linearaccelerationsensor-screen.json"
+              referenceFrame="screen"
+              ?active="${this.page === 'linearaccelerationsensor-screen'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="Gyroscope"
+              src="src/tests/gyroscope.json"
+              ?active="${this.page === 'gyroscope'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="AbsoluteOrientationSensor"
+              src="src/tests/absoluteorientationsensor.json"
+              ?active="${this.page === 'absoluteorientationsensor'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="RelativeOrientationSensor"
+              src="src/tests/relativeorientationsensor.json"
+              ?active="${this.page === 'relativeorientationsensor'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="Magnetometer"
+              src="src/tests/magnetometer.json"
+              frequency="10"
+              ?active="${this.page === 'magnetometer'}">
+            </sensor-tests-page>
+            <sensor-tests-page class="page"
+              type="AmbientLightSensor"
+              src="src/tests/ambientlightsensor.json"
+              frequency="10"
+              ?active="${this.page === 'ambientlightsensor'}">
+            </sensor-tests-page>
+          </main>
+        </app-header-layout>
+      </app-drawer-layout>
+    `;
+  }
+}
+
+window.customElements.define('sensor-tester', SensorTester);
---
+++ sensor-tester/src/sensor-tests-page.js
@@ -0,0 +1,272 @@
+import {LitElement, html} from '@polymer/lit-element';
+import "@material/mwc-button";
+import "@polymer/paper-spinner/paper-spinner-lite";
+import "@polymer/paper-styles/color";
+import "./orientation-changer.js";
+import "./lazy-image.js"
+
+class SensorTestsPage extends LitElement {
+  static get properties() {
+    return {
+      frequency: {type: Number},
+      referenceFrame: {type: String},
+      type: {type: String},
+      src: {type: String},
+      isSupported: {type: String},
+      items: {type: Array}
+    };
+  }
+
+  constructor() {
+    super();
+    this.frequency = 60;
+    this.referenceFrame = "device";
+    this.isSupported = "supported";
+    this.items = [];
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const target = entry.target;
+          fetch(this.src).then(response => response.json()).then(json => {
+            this.items = json;
+            console.log("Loading data file:", this.src)
+            this.requestUpdate('items');
+          });
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+    observer.observe(this);
+  }
+
+  get sensor() {
+    return this.sensor_;
+  }
+
+  getItemText(state) {
+    return state || "RUN";
+  }
+
+  isTestRunning(state) {
+    return state === "RUNNING";
+  }
+
+  runAllTests() {
+    // TODO(alex)
+  }
+
+  testRunning(index) {
+    this.items[index].state = 'RUNNING';
+    this.requestUpdate('items');
+  }
+
+  testPassed(index) {
+    this.items[index].state = 'PASSED';
+    this.requestUpdate('items');
+    this.testCompleted(index);
+  }
+
+  testFailed(index) {
+    this.items[index].state = 'FAILED';
+    this.requestUpdate('items');
+    this.testCompleted(index);
+  }
+
+  testCompleted(index) {
+    clearTimeout(this.items[index].timeout_id);
+    this.sensor.removeEventListener('reading', this.items[index].handler);
+    this.sensor.stop();
+  }
+
+  firstUpdated() {
+    if (this.type in window) {
+      this.sensor_ = new window[this.type]({
+        frequency: this.frequency,
+        referenceFrame: this.referenceFrame
+      });
+
+      this.sensor_.onreading = () => {
+        this.sensor_.stop();
+        this.sensor_.onreading = null;
+      }
+
+      this.sensor_.onerror = e => {
+        if (e.error.name === 'NotReadableError') {
+          this.isSupported = "not-supported";
+          this.referenceFrame = "device";
+          this.items = [];
+          this.requestUpdate('items');
+        }
+      }
+
+      this.sensor_.start();
+    } else {
+      this.isSupported = "not-supported";
+      this.referenceFrame = "device";
+    }
+  }
+
+  runTest(index) {
+    let item = this.items[index];
+
+    // set state of test case to 'running'
+    this.testRunning(index);
+
+    // set timeout and save it's id
+    this.items[index].timeout_id = setTimeout(() => {
+      this.testFailed(index);
+    }, item.duration * 1000);
+
+    const compareReadings = (val, exp, eps) => {
+      if (typeof exp == 'object' && "min" in exp) {
+        return val >= exp.min;
+      }
+      else if (typeof exp == 'object' && "max" in exp) {
+        return val <= exp.max;
+      }
+      else if (val instanceof Array && exp instanceof Array) {
+        if (val.length != exp.length) {
+            return false;
+        }
+        return Object.keys(val).every(key => compareReadings(val[key], exp[key], eps));
+      } else if (typeof exp === 'number' && typeof val === 'number' ) {
+        return Math.abs(val - exp) < eps;
+      }
+
+      return false;
+    }
+
+    // if sensor value is same as expectation, pass test
+    this.items[index].handler = () => {
+      if (Object.keys(item.expected).every(
+          key => compareReadings(this.sensor[key], item.expected[key], item.epsilon))) {
+          this.testPassed(index);
+      }
+    }
+
+    // add onreading event listener to receive new events.
+    this.sensor.addEventListener('reading', this.items[index].handler);
+
+    // start sensor
+    this.sensor.start();
+  }
+
+  render() {
+    return html`
+      <style>
+        :host {
+          display: block;
+          padding: 10px;
+        }
+  
+        div.not-supported {
+          display: block;
+        }
+  
+        div.supported {
+          display: none;
+        }
+  
+        div.device {
+          display: none;
+        }
+  
+        div.screen {
+          display: block;
+        }
+  
+        mwc-button {
+          margin-left: 0px;
+          margin-right: 10px;
+          position: relative;
+          float: right;
+          --mdc-theme-primary: var(--app-primary-color);
+        }
+  
+        mwc-button.LOCK {
+          margin-bottom: 10px;
+          float: none;
+          --mdc-theme-primary: var(--app-primary-color);
+        }
+  
+        mwc-button.PASSED {
+          --mdc-theme-primary: var(--paper-green-500);
+        }
+  
+        mwc-button.FAILED {
+          --mdc-theme-primary: var(--paper-red-500);
+        }
+  
+        mwc-button.RUNNING {
+          --mdc-theme-primary: var(--app-primary-color);
+        }
+  
+        paper-spinner-lite.green {
+          margin: 6px 10px 0px 0px;
+          --paper-spinner-color: var(--paper-green-500);
+        }
+  
+        .item {
+          @apply --layout-vertical;
+          background-color: white;
+          max-width: 100%;
+          padding: 15px;
+          margin-bottom: 10px;
+          border-radius: 8px;
+          border: 1px solid #ddd;
+          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+        }
+  
+        .header {
+          @apply --layout-horizontal;
+        }
+  
+        h1 {
+          margin: 10px 0 0 0;
+          font-size: 20px;
+        }
+  
+        .title {
+          height: 40px;
+          @apply --layout-flex;
+        }
+  
+        .description {
+          margin-top: 14px;
+        }
+  
+        .image {
+          @apply --layout-horizontal;
+          @apply --layout-wrap;
+          @apply --layout-center-justified;
+          margin-top: 8px;
+        }
+      </style>
+  
+      <div class="${this.isSupported}">Sensor is not supported by the browser or the device.</div>
+
+      <div class="${this.referenceFrame}">
+        <orientation-changer></orientation-changer>
+      </div>
+
+      ${this.items.map((item, index) => html`
+          <div>
+            <div class="item">
+              <div class="header">
+                <div class="title"><h1>${item.name}</h1></div>
+                <paper-spinner-lite ?active="${this.isTestRunning(item.state)}" class="green"></paper-spinner-lite>
+                <mwc-button raised @click="${() => this.runTest(index)}" class="${item.state}" label="${this.getItemText(item.state)}"></mwc-button>
+              </div>
+              <div class="description">${item.description}</div>
+              <div class="image">
+                <lazy-image src="${item.illustration}"></lazy-image>
+              </div>
+            </div>
+          </div>
+      `)}
+    `;
+  }
+}
+
+customElements.define('sensor-tests-page', SensorTestsPage);
